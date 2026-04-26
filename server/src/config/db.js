4c5ac41dfaf7fc492fa78cf1c61c9c0a71d7f5ea
cse1112 <sohailjustin40@gmail.com>
2026-04-27 00:05:46 +0530

    debug mongo uri

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,9 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 async function connectDB() {
-    await mongoose.connect(process.env.MONGO_URI)
+  try {
+    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+    console.log("MONGO_URI preview:", process.env.MONGO_URI?.replace(/:\/\/(.*?):(.*?)@/, "://$1:****@"));
 
-    console.log("Connected to DataBase")  
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to DataBase");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
 }
 
-module.exports = connectDB
+module.exports = connectDB;
