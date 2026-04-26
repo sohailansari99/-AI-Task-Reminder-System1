5c1755e55b4228f40fe5e5c721796858e544c2a5
cse1112 <sohailjustin40@gmail.com>
2026-04-26 18:29:44 +0530

    force update frontend serving

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 
-const app = require("./src/app");
-const connectDB = require("./src/config/db");
-const startReminderCron = require("./src/jobs/reminderCron");
-const startPriorityCron = require("./src/jobs/priorityCron");
+const app = require("./server/src/app");  // 👈 MUST be this
+
+const connectDB = require("./server/src/config/db");
+const startReminderCron = require("./server/src/jobs/reminderCron");
+const startPriorityCron = require("./server/src/jobs/priorityCron");
 
 connectDB();
-
 startReminderCron();
 startPriorityCron();
 
-const PORT = process.env.PORT || 3600;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-  console.log(`Server is runnig on ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
